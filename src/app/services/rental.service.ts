33cb4697c0f5feb5625a1996be5c64d7152d5ab8
Mert patlar <meertpatlar@gmail.com>
2022-07-27 23:06:27 +0300

    Rent services adding got many errors

--- src/app/services/rental.service.ts
+++ src/app/services/rental.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { CreditCard } from '../models/CreditCard';
 import { listResponseModel } from '../models/listResponseModel';
 import { Rental } from '../models/Rental';
+import { ResponseModel } from '../models/ResponseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +12,22 @@ import { Rental } from '../models/Rental';
 export class RentalService {
 
   apiUrl = "https://localhost:44371/api/rentals/getrentaldetaildto"
+  serviceUrl: string;
 
   constructor(private httpclient:HttpClient) { }
 
-  getRentals():Observable<listResponseModel<Rental>>{
-    return this.httpclient.get<listResponseModel<Rental>>(this.apiUrl);
+
+  getRentalDetails(): Observable<listResponseModel<RentalDetail>>{
+    return this.httpclient.get<listResponseModel<RentalDetail>>(this.serviceUrl + "getdetails")
   }
+  getOccupiedDates(carId:number): Observable<listResponseModel<Date>>{
+    return this.httpclient.get<listResponseModel<Date>>(this.serviceUrl + "getoccupieddates?carId=" + carId )
+  }
+
+  rent(rental:Rental, creditCard:CreditCard): Observable<ResponseModel>{
+    return this.httpclient.post<ResponseModel>(this.serviceUrl + "add", {rental, creditCard})
+
+  }
+
 
 }
